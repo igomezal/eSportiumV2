59d1463bc29041a08d5899f2010651701c65cdea
Peropoko <cosasystuff@hotmail.com>
2016-02-28 19:23:49 +0100

    Se comprueba la contraseña según se escribe

--- js/jS.js
+++ js/jS.js
@@ -1,5 +1,6 @@
 $(document).ready( function (){
-    $("#btnEnviar").click(checkPassword);
+    $("#InputPassword1").keyup(checkPassword);
+    $("#InputPasswordConfirm1").keyup(checkPassword);
 });
 
 var checkPassword = function (){
@@ -8,20 +9,24 @@ var checkPassword = function (){
         $("#divPassword #errortxt").remove();
         $("#divPassword").addClass("has-error");
         $("#divPassword").append("<p id=\"errortxt\"><u><strong>La contraseña debe tener mínimo 6 caracteres</strong></u></p>");
+        $("#btnEnviar").prop("disabled", true);
     }else{
         $("#divPassword #errortxt").remove();
         $("#divPassword").removeClass("has-error");
+        $("#btnEnviar").prop("disabled", false);
     }
     var pass2 = $("#InputPasswordConfirm1").val();
     if (pass1 === pass2){
         $("#divConfirm #errortxt").remove();
         $("#divConfirm").removeClass("has-error");
-        if(pass1.length < 6){
+        $("#btnEnviar").prop("disabled", false);
+        if(pass1.length > 6){
             //Cargar la página con el usuario registrado
         }
     }else{
         $("#divConfirm").addClass("has-error");
         $("#divConfirm #errortxt").remove();
         $("#divConfirm").append("<p id=\"errortxt\"><u><strong>Las contraseñas no coinciden</strong></u></p>");
+        $("#btnEnviar").prop("disabled", true);
     }
 }
