f39498bde98674045fc80dd5890ef6219bc3d93f
Iván Gómez Alonso <yvangomezalonso@gmail.com>
2016-05-23 18:46:59 +0200

    Añadida clase Partido

---
+++ fase4/src/main/java/es/urjc/code/daw/eSportium/partido/Partido.java
@@ -0,0 +1,15 @@
+package es.urjc.code.daw.eSportium.partido;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Partido{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+}
